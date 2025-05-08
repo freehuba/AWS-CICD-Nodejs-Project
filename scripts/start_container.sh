c98f1458e1d46c793d46aebbd210592268acdfac
Hemanth j m <79273699+freehuba@users.noreply.github.com>
2025-05-08 17:32:32 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,6 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull hemanthhemanthjm230623/simple-nodejs-app
 
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 hemanthhemanthjm230623/simple-nodejs-app
